c2fb5585a8731f69ca4a58170ee3f020811846ba
Fabio752 <fabiodeo752@gmail.com>
2019-10-17 11:14:55 +0100

    Moving a certain distance

--- basic_run.py
+++ basic_run.py
@@ -1,10 +1,22 @@
 # Write your code here :-)
 import brickpi3
 BP = brickpi3.BrickPi3()
+
+def get_encode_length(distance):
+    return (distance / 22)*450
+
 try:
-    while True:
-    	BP.set_motor_power(BP.PORT_A, 50)
-    	BP.set_motor_power(BP.PORT_B, 50)
+    BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
+    BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
+    target_degree_rotation = get_encode_length(40)
+    BP.set_motor_dps(BP.PORT_A, 90)
+    BP.set_motor_dps(BP.PORT_B, 90)
+    actual_degree = 0
+    while actual_degree < target_degree_rotation:
+    	actual_degree = (BP.get_motor_encoder(BP.PORT_A) + BP.get_motor_encoder(BP.PORT_B)) / 2
+	print("A: %s, B: %s" % (BP.get_motor_encoder(BP.PORT_A), BP.get_motor_encoder(BP.PORT_B)))
 
 except KeyboardInterrupt:
     BP.reset_all()
+    BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A))
+    BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B))
